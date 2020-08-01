cb257c47e9fed2d56a0306dd42e9a8fe29cb4d18
Nick Zaytsev <exluap@icloud.com>
2020-08-02 00:42:24 +0300

    MW-1 Добавлен шаблон миграций

---
+++ internal/database/migrations.go
@@ -0,0 +1,11 @@
+/**
+ * Project api-microworld created by exluap
+ * Date: 02.08.2020 00:39
+ * twitter: https://twitter.com/exluap
+ * keybase: https://keybase.io/exluap
+ * website: https://exluap.com
+ */
+
+// Миграции в БД для дальнейшей работы
+
+package database
